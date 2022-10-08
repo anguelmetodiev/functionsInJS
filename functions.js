fc717d8127fb619dcf72f2a52fd67f2c3d45d256
Anguel M. Metodiev <anguelmetodiev@gmail.com>
2022-10-07 21:53:12 -0400

    Question 2 has been added.

--- functions.js
+++ functions.js
@@ -14,3 +14,20 @@ function maxOfTwoNumbers(x, y) {
   console.log(maxOfTwoNumbers(3, 9)); // returns 9
   console.log(maxOfTwoNumbers(-5, 12)); // returns 12
   console.log(maxOfTwoNumbers(4, 0)); // returns 4
+
+console.log("Question 2:")
+// 2.
+const maxOfThree = function(numberOne, numberTwo, numberThree) {
+    if ((numberOne >= numberTwo) && (numberOne >= numberThree)) { 
+        return numberOne;
+    } else if ((numberTwo >= numberOne) && (numberTwo >= numberThree)) {
+        return numberTwo;
+    } else {
+        return numberThree;
+    }
+  }
+  
+  console.log(maxOfThree(3, 4, 5)) // returns 5
+  console.log(maxOfThree(6, 7, 9)) // returns 9
+  console.log(maxOfThree(0, -2, 4)) // returns 4
+  console.log(maxOfThree(3, 3, 3)) // returns 3
