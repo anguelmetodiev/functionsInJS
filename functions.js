eb2180634ced20c1f8aa8781c97525445a586de7
Anguel M. Metodiev <anguelmetodiev@gmail.com>
2022-10-07 22:05:05 -0400

    Question 9 has been added.

--- functions.js
+++ functions.js
@@ -2,41 +2,41 @@ console.log("Question 1:")
 // 1.
 function maxOfTwoNumbers(x, y) {
     if (x >= y) {
-      return x;
+        return x;
     } else {
-      return y;
+        return y;
     }
-  
+
     // or more "elegantly" using the fantastic ternary expression!
     // return  x >= y ? x : y;
-  }
-  
-  console.log(maxOfTwoNumbers(3, 9)); // returns 9
-  console.log(maxOfTwoNumbers(-5, 12)); // returns 12
-  console.log(maxOfTwoNumbers(4, 0)); // returns 4
+}
+
+console.log(maxOfTwoNumbers(3, 9)); // returns 9
+console.log(maxOfTwoNumbers(-5, 12)); // returns 12
+console.log(maxOfTwoNumbers(4, 0)); // returns 4
 
 console.log("Question 2:")
 // 2.
-const maxOfThree = function(numberOne, numberTwo, numberThree) {
-    if ((numberOne >= numberTwo) && (numberOne >= numberThree)) { 
+const maxOfThree = function (numberOne, numberTwo, numberThree) {
+    if ((numberOne >= numberTwo) && (numberOne >= numberThree)) {
         return numberOne;
     } else if ((numberTwo >= numberOne) && (numberTwo >= numberThree)) {
         return numberTwo;
     } else {
         return numberThree;
     }
-  }
-  
-  console.log(maxOfThree(3, 4, 5)) // returns 5
-  console.log(maxOfThree(6, 7, 9)) // returns 9
-  console.log(maxOfThree(0, -2, 4)) // returns 4
-  console.log(maxOfThree(3, 3, 3)) // returns 3
+}
+
+console.log(maxOfThree(3, 4, 5)) // returns 5
+console.log(maxOfThree(6, 7, 9)) // returns 9
+console.log(maxOfThree(0, -2, 4)) // returns 4
+console.log(maxOfThree(3, 3, 3)) // returns 3
 
 console.log("Question 3:")
 // 3.
 function isCharAVowel(character) {
     character = character.toLowerCase()
-    if (character ==="a" || character ==="e" || character ==="i" || character ==="o" || character ==="u" ){
+    if (character === "a" || character === "e" || character === "i" || character === "o" || character === "u") {
         return true
     } else {
         return false
@@ -51,29 +51,29 @@ console.log("Question 4:")
 // 4.
 const sumArray = (array) => {
     sum = 0
-    for (i=0; i < array.length; i++) {
+    for (i = 0; i < array.length; i++) {
         sum += array[i]
     }
     return sum
 }
 
-console.log(sumArray([1,2,3,4])) // returns 10
-console.log(sumArray([2,4,5])) // returns 11
-console.log(sumArray([4,1])) // returns 5
+console.log(sumArray([1, 2, 3, 4])) // returns 10
+console.log(sumArray([2, 4, 5])) // returns 11
+console.log(sumArray([4, 1])) // returns 5
 
 console.log("Question 5:")
 // 5.
 function multiplyArray(array) {
     result = 1
-    for (i=0; i < array.length; i++) {
+    for (i = 0; i < array.length; i++) {
         result *= array[i]
     }
     return result
 }
 
-console.log(multiplyArray([2,4,5])) // returns 40
-console.log(multiplyArray([2,4])) // returns 8
-console.log(multiplyArray([3,5,2,4])) // returns 120
+console.log(multiplyArray([2, 4, 5])) // returns 40
+console.log(multiplyArray([2, 4])) // returns 8
+console.log(multiplyArray([3, 5, 2, 4])) // returns 120
 
 console.log("Question 6:")
 // 6.
@@ -89,10 +89,10 @@ console.log("Question 7:")
 // 7.
 function reverseStraight(str) {
     let reversedString = '';
-         for (i = str.length - 1; i >= 0; i--){
-            reversedString = reversedString + str[i];
-     }
-     return reversedString;
+    for (i = str.length - 1; i >= 0; i--) {
+        reversedString = reversedString + str[i];
+    }
+    return reversedString;
 }
 
 // ======= Using Methods =========
@@ -122,3 +122,19 @@ const longestStringInArray = (array) => {
 longestStringInArray(["hi", "bye", "technical"]) // returns 9
 longestStringInArray(["anguel", "test", "program"]) // returns 7
 longestStringInArray(["aaaaaa", "bbbbb", "cc"]) // returns 6
+
+console.log("Question 9:")
+// 9.
+function stringsLongerThan(strArray, num) {
+    const resultArray = [];
+    for (let i = 0; i < strArray.length; i++) {
+        if (strArray[i].length > num) {
+            resultArray.push(strArray[i])
+        }
+    }
+    return resultArray
+}
+
+console.log(stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3)); // returns ['hello' , 'morning']
+console.log(stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 5)); // returns ['morning]
+console.log(stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 7)); // returns []
